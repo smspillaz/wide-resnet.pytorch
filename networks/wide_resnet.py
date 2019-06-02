d550b1aa65f7ace02f4646f13cdffbb3f7ad4bdb
Sam Spilsbury <smspillaz@gmail.com>
2019-06-02 13:27:17 +0300

    wide_resnet: Parameterize in_channels

--- networks/wide_resnet.py
+++ networks/wide_resnet.py
@@ -42,7 +42,7 @@ class wide_basic(nn.Module):
         return out
 
 class Wide_ResNet(nn.Module):
-    def __init__(self, depth, widen_factor, dropout_rate, num_classes):
+    def __init__(self, depth, widen_factor, dropout_rate, in_channels, num_classes):
         super(Wide_ResNet, self).__init__()
         self.in_planes = 16
 
@@ -53,7 +53,7 @@ class Wide_ResNet(nn.Module):
         print('| Wide-Resnet %dx%d' %(depth, k))
         nStages = [16, 16*k, 32*k, 64*k]
 
-        self.conv1 = conv3x3(3,nStages[0])
+        self.conv1 = conv3x3(in_channels,nStages[0])
         self.layer1 = self._wide_layer(wide_basic, nStages[1], n, dropout_rate, stride=1)
         self.layer2 = self._wide_layer(wide_basic, nStages[2], n, dropout_rate, stride=2)
         self.layer3 = self._wide_layer(wide_basic, nStages[3], n, dropout_rate, stride=2)
